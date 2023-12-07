6d742fd4dfe9f34261dd872f9cc43604021451e3
yash-bhoskar-automatonai <yash.bhoskar@automatonai.com>
2023-12-07 16:25:56 +0530

    Careers page content added

--- autoGitPush.sh
+++ autoGitPush.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 git add .
 git commit -m "Careers page content added"
-printf 'yashbhoskar\n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' | git push origin main
+expect << EOF
+spawn git push origin main
+expect "Username for 'https://github.com':"
+send "yashbhoskar\r"
+expect "Password for 'https://your_username@github.com':"
+send "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\r"
+expect eof
+EOF
